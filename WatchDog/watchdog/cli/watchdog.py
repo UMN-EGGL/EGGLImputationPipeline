29672acd04a769dced1bb23e30b1da7673488807
Rob <schae234@umn.edu>
2019-12-30 10:58:00 -0600

    finish rule for phasing

--- WatchDog/watchdog/cli/watchdog.py
+++ WatchDog/watchdog/cli/watchdog.py
@@ -5,22 +5,35 @@ from watchdog.watcher import watch_beagle
 
 @click.command()
 @click.option('--gt')
-@click.option('--out')
-@click.option('--window')
-@click.option('--overlap')
-@click.option('--nthreads')
-def cli(gt,out,window,overlap,nthreads):
+@click.option('--out-prefix')
+@click.option('--window', default=0.05)
+@click.option('--overlap', default=0.005)
+@click.option('--nthreads', default=4)
+@click.option('--timeout',default=10)
+@click.option('--debug/--no-debug', default=False)
+@click.option('--heap-size', default='50g')
+def cli(
+        gt,out_prefix,window,overlap,
+        nthreads,timeout,heap_size,debug
+    ):
     """
         [ WD ] : WatchDog
     """
+    if debug:
+        from IPython.core import ultratb
+        sys.excepthook = ultratb.FormattedTB(
+            mode='Verbose', color_scheme='Linux', call_pdb=1
+        )
+
     command_exit_code = asyncio.run(
         watch_beagle(
             gt, 
-            out,
-            impute='true',
+            out_prefix,
             window=window,
             overlap=overlap,
             nthreads=nthreads,
+            timeout=timeout,
+            heap_size=heap_size
         )
     )
     sys.exit(command_exit_code)
--- WatchDog/watchdog/watcher.py
+++ WatchDog/watchdog/watcher.py
@@ -2,25 +2,97 @@ import os
 import re
 import asyncio
 import tempfile
+import subprocess
+import numpy as np
 from asyncio.subprocess import PIPE, STDOUT
 
 
+from .exceptions import PhasingTimeoutError
+
 async def watch_beagle(
         gt,
-        out,
-        impute='true',
+        out_prefix,
+        window=0.05,
+        overlap=0.005,
+        nthreads=4,
+        timeout=60, 
+        check_every=5,
+        heap_size='50g'
+    ):
+    # set some starting parameters
+    input_vcf = gt
+    filtered_vcf = None 
+    # Loop and try to phase
+    while True:
+        try:
+            phase_success = await phase_vcf(
+                gt=input_vcf, out_prefix=out_prefix, 
+                window=window, overlap=overlap, 
+                nthreads=nthreads, timeout=timeout,
+                check_every=check_every,
+                heap_size=heap_size
+            )
+            if phase_success:
+                break
+            else:
+                raise 
+        except PhasingTimeoutError as e:
+            # filter the vcf
+            filtered_vcf = filter_window(
+                input_vcf,
+                e.window_chrom,
+                e.window_start,
+                e.window_end
+            )
+            # This will delete the previous temp file
+            try: 
+                input_vcf.close()
+            except AttributeError:
+                pass
+            input_vcf = filtered_vcf.name
+
+async def phase_vcf(
+        gt,
+        out_prefix,
         window=0.05,
         overlap=0.005,
         nthreads=4,
         timeout=30, 
-        check_every=5):
+        check_every=5,
+        heap_size='50g'
+    ):
     '''
-        asyncronously run beagle and watch its output waiting for it to die
+        Attempts to phase a VCF file using BEAGLE. The function monitors STDOUT and
+        if BEAGLE stalls on producing output for longer than the `timeout` parameter,
+        the process is killed.
+
+        Parameters
+        ----------
+        gt: (pathlike string)
+            input VCF file to be phased
+        out_prefix: (pathlike string)
+            the prefix string for the output files.
+            Will produce {out_prefix}.vcf.gz and 
+            {out_prefix}.log
+        window: float
+            The size of the imputation window (in megabases)
+        overlap: float
+            The size of the overlap window (in megabases)
+        nthreads: int
+            The number of threads used to phase
+        timeout: int
+            The number of total seconds for the function to 
+            wait on a window before a timeout occurs
+        check_every: int
+            The number of seconds to check
+        heap_size: (default: 50g)
+            The size of the java heap. Passed 
+            to the java -Xmx parameter.
     '''
     beagle_jar = os.environ['BEAGLE_JAR']
     cmd = [
-        'java', '-jar', beagle_jar, 
-        f'gt={gt}', f'out={out}', f'impute={impute}', 
+        'java', f'-Xmx{heap_size}', '-jar', beagle_jar,  
+        f'gt={gt}', f'out={out_prefix}', f'impute=true', 
         f'window={window}', f'overlap={overlap}',
         f'nthreads={nthreads}'
     ]
@@ -47,8 +119,17 @@ async def watch_beagle(
             if  total_waiting >= timeout:
                 print(f"[ WD ]: BEAGLE TIMED OUT PROCESSING {window_chrom}:{window_start}-{window_end}")
                 process.kill()
-                return_code = -9
-                break
+                # Remove the temp output files
+                if os.path.exists(out_prefix+'.vcf.gz'):
+                    os.remove(out_prefix+'.vcf.gz')
+                if os.path.exists(out_prefix+'.log'):
+                    os.remove(out_prefix+'.log')
+                raise PhasingTimeoutError(
+                    f"Timeout out on the window {window_chrom}:{window_start}-{window_end}",
+                    window_chrom,
+                    window_start,
+                    window_end
+                )
         else:
             if not line:
                 break # End of File
@@ -60,36 +141,79 @@ async def watch_beagle(
                 # reset the timeout
                 total_waiting = 0
                 # Print the output
-                print(f"[ WD ]: Found the output: {line}",end="",flush=True)
+                print(f"[ WD ]: {line}",end="",flush=True)
                 # continue the loop
                 continue
     # wait for the child to exit
     await process.wait()
     # return the code
-    return return_code
-
-
+    return True
 
 def filter_window(input_vcf, window_chrom, window_start, window_end, threshold=0.95):
-    import subprocess
-    new_vcf = tempfile.NamedTemporaryFile('w') 
-    print(f"Creating new VCF: {new_vcf.name}")
+    '''
+    Returns a named temp file containing the filtered 
+    '''
+    window_start = int(window_start)
+    window_end  = int(window_end)
+
+    new_vcf = tempfile.NamedTemporaryFile('w',suffix='.vcf',delete=True) 
+
+    print(f"[ WD ]: Creating new VCF: {new_vcf.name}")
+    # Index if not there ---------------------------------------------
+    if not os.path.exists(input_vcf+'.csi'):
+        print(f"[ WD ]: Indexing {input_vcf}")
+        cmd = f'bcftools index {input_vcf}'.split(' ')
+        index = subprocess.run(
+            cmd, capture_output=True
+        )  
     # Print the header -----------------------------------------------
     cmd = f'bcftools view {input_vcf} -r {window_chrom}:{0}-{max(0,window_start-1)}'.split(' ')
-    print(cmd)
-    header = subprocess.Popen(
-        cmd, stdout=subprocess.PIPE     
+    print("[ WD ]: Printing header")
+    header = subprocess.run(
+        cmd, capture_output=True, encoding='utf-8',text=True
     )
-    for line in header.communicate()[0].splitlines():
-        print(line.decode(), file=new_vcf, flush=True)
+    for line in header.stdout.strip().split('\n'):
+        print(line, file=new_vcf, flush=True)
     # Process the window ---------------------------------------------
     cmd = f'bcftools view -H {input_vcf} -r {window_chrom}:{window_start}-{window_end}'.split(' ')
-    print(cmd)
-    window = subprocess.Popen(
-        cmd, stdout=subprocess.PIPE     
+    print(f"[ WD ]: Processing window: {window_chrom}:{window_start}-{window_end}")
+    window = subprocess.run(
+        cmd, capture_output=True, encoding='utf-8',text=True
     )
-    lines =  [x.decode() for x in window.communicate()[0].splitlines()]
+    lines =  [x for x in window.stdout.strip().split('\n')]
+    print(f"[ WD ]: There are {len(lines)} in the window")
     # Filter out the lowest x% of VQSLOD scores
-    # [{x:y for x,y in map(lambda x: x.split('='),line.split('\t')[7].split(';'))}  for line in lines]
-    breakpoint()
-    x=1
+    LOD_scores = []
+    for line in lines:
+        info_fields = line.split('\t')[7].split(';') 
+        for k,v in map(lambda x: x.split('='), info_fields):
+            if k == 'VQSLOD':
+                LOD_scores.append(float(v))
+    quantile_cutoff = np.quantile(LOD_scores, 1-threshold)
+    print(f"[ WD ]: Filtering out variants with VQSLOD < {quantile_cutoff}")
+    num_filtered  = 0
+    for line,lod in zip(lines,LOD_scores):
+        if lod >= quantile_cutoff:
+            print(line, file=new_vcf,flush=True)
+        else:
+            num_filtered += 1
+    print(f"[ WD ]: Filtered out {num_filtered} variants in window ({100*(num_filtered/len(lines)):.2f}%)")
+    # Process the rest ------------------------------------------------
+    cmd = f'bcftools view -H {input_vcf} -r {window_chrom}:{window_end+1}-'.split(' ')
+    print(f"[ WD ]: Printing out rest of variants")
+    header = subprocess.run(
+        cmd, capture_output=True, encoding='utf-8',text=True
+    )
+    for line in header.stdout.strip().split('\n'):
+        print(line, file=new_vcf, flush=True)
+    
+    new_bgzip = tempfile.NamedTemporaryFile('w',suffix='.vcf.gz',delete=True)
+    cmd = f'bcftools view {new_vcf.name} -Oz -o {new_bgzip.name}'.split(' ')
+    print(f"[ WD ]: compressing {new_vcf.name} into {new_bgzip.name}")
+    window = subprocess.run(
+        cmd, capture_output=True, encoding='utf-8',text=True
+    )
+    print(f"[ WD ]: Closing {new_vcf.name}")
+    new_vcf.close()
+
+    return new_bgzip
