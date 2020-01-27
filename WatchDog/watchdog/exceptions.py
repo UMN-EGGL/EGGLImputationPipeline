f284d39a2b7ffec36259c4bbc8e9db344f7beb9f
Rob <schae234@umn.edu>
2020-01-27 11:29:24 -0600

    dont pass variables through exception

--- WatchDog/watchdog/exceptions.py
+++ WatchDog/watchdog/exceptions.py
@@ -1,7 +1,4 @@
-class PhasingTimeoutError(Exception):
-    def __init__(self, msg, window_chrom, window_start, window_end):
+class BeagleTimeoutError(Exception):
+    def __init__(self, msg=''):
         super().__init__(msg)
-        self.window_chrom = window_chrom
-        self.window_start = window_start
-        self.window_end   = window_end
 
