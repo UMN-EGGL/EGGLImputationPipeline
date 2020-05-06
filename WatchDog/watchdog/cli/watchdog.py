f4e63e429be25c0d7725f1803ec14000925e08ad
Rob <schae234@umn.edu>
2020-05-06 12:16:52 -0500

    add options for specifying a locuspocus db for dropped SNPs

--- WatchDog/watchdog/cli/watchdog.py
+++ WatchDog/watchdog/cli/watchdog.py
@@ -2,7 +2,7 @@ import sys
 import click
 import asyncio
 import shutil
-from watchdog.watcher import watcher
+from watchdog import Watcher
 
 @click.command()
 @click.option('--gt',type=click.Path(exists=True))
@@ -14,9 +14,12 @@ from watchdog.watcher import watcher
 @click.option('--timeout',default=30)
 @click.option('--debug/--no-debug', default=False)
 @click.option('--heap-size', default='50g')
+@click.option('--locuspocus-name', default=None)
+@click.option('--locuspocus-basedir', default=None)
 def cli(
         gt,out_prefix,ref,window,overlap,
-        nthreads,timeout,heap_size,debug
+        nthreads,timeout,heap_size,debug,
+        locuspocus_name,locuspocus_basedir
     ):
     """
         [ WD ] : WatchDog
@@ -31,7 +34,7 @@ def cli(
         sys.excepthook = ultratb.FormattedTB(
             mode='Verbose', color_scheme='Linux', call_pdb=1
         )
-    w = watcher(
+    w = Watcher(
         gt, 
         out_prefix,
         ref=ref,
@@ -39,7 +42,9 @@ def cli(
         overlap=overlap,
         nthreads=nthreads,
         timeout=timeout,
-        heap_size=heap_size
+        heap_size=heap_size,
+        locuspocus_name=locuspocus_name,
+        locuspocus_basedir=locuspocus_basedir
     )
     command_exit_code = asyncio.run(w.run())
     sys.exit(command_exit_code)
